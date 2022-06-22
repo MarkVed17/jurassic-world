f5849636bf6c696c48bba09e7590465bdadf55a8
MarkVed17 <vedantlahane17@gmail.com>
2022-06-22 23:11:20 +0530

    added barrel exports

---
+++ src/frontend/pages/index.js
@@ -0,0 +1,7 @@
+export { FeedPage } from "./FeedPage";
+export { BookmarksPage } from "./BookmarksPage";
+export { ProfilePage } from "./ProfilePage";
+export { ExplorePage } from "./ExplorePage";
+export { SinglePostPage } from "./SinglePostPage";
+export { SignInPage } from "./SignInPage";
+export { SignUpPage } from "./SignUpPage";
