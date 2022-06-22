c5c6eb957f657b01154aa1aa6a57b217889fb7dd
MarkVed17 <vedantlahane17@gmail.com>
2022-06-23 00:19:47 +0530

    updated `Single Post Page`

--- src/frontend/pages/SinglePostPage/SinglePostPage.js
+++ src/frontend/pages/SinglePostPage/SinglePostPage.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Post, Sidebar, SuggestedUsersCard } from "../../components";
+import {
+  Comment,
+  CommentSection,
+  Post,
+  Sidebar,
+  SuggestedUsersCard,
+} from "../../components";
 
 const SinglePostPage = () => {
   return (
@@ -8,12 +14,17 @@ const SinglePostPage = () => {
       <Sidebar />
 
       {/* Center Content */}
-      <div className="flex flex-col grow gap-3 max-w-2xl p-4">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+      <div className="flex flex-col grow max-w-2xl p-4">
+        <div className="flex flex-col gap-3">
+          <Post />
+          <CommentSection />
+          {/* Comments */}
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
       </div>
 
       {/* Suggested Users */}
