0431386e2d2471ff97d5ea8f7e13a0eaad848a6c
MarkVed17 <vedantlahane17@gmail.com>
2022-06-22 23:06:08 +0530

    feat: created `Sidebar`

---
+++ src/frontend/components/Sidebar.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  const linkStyle = ({ isActive }) =>
+    isActive
+      ? "flex items-center p-2 text-lg font-normal text-gray-900 rounded-lg dark:text-white bg-gray-100 dark:bg-gray-700"
+      : "flex items-center p-2 text-lg font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700";
+
+  return (
+    <aside className="hidden w-64 h-full sticky top-20 lg:block">
+      <div className="overflow-y-auto py-4 px-3 bg-gray-50 dark:bg-gray-800">
+        <ul className="space-y-2">
+          <li>
+            <NavLink to="/" className={linkStyle}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+              </svg>
+              <span className="ml-3">Feed</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/explore" className={linkStyle}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="flex-1 ml-3 whitespace-nowrap">Explore</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/bookmarks" className={linkStyle}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+              </svg>
+              <span className="flex-1 ml-3 whitespace-nowrap">Bookmarks</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile" className={linkStyle}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="flex-1 ml-3 whitespace-nowrap">Profile</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export { Sidebar };
