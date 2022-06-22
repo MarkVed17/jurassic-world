6d93f0c194254c478a0f9e6309c595e11f737a6c
MarkVed17 <vedantlahane17@gmail.com>
2022-06-22 23:06:56 +0530

    added barrel exports

---
+++ src/frontend/components/index.js
@@ -0,0 +1,7 @@
+export { Sidebar } from "./Sidebar";
+export { MobileNav } from "./MobileNav";
+export { SuggestedUsersCard } from "./SuggestedUsersCard";
+export { CreatePost } from "./CreatePost";
+export { ProfileSection } from "./ProfileSection";
+export { Post } from "./Post";
+export { NavProfileDropdown } from "./NavProfileDropdown";
