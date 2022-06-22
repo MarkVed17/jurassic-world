03b324fd69ecf348be7fe5f460fb414b368eb13e
MarkVed17 <vedantlahane17@gmail.com>
2022-06-22 23:08:15 +0530

    feat: created `Bookmarks Page`

---
+++ src/frontend/pages/BookmarksPage/BookmarksPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Post, Sidebar, SuggestedUsersCard } from "../../components";
+
+const BookmarksPage = () => {
+  return (
+    <div className="main-content justify-evenly dark:bg-gray-800">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Center Content */}
+      <div className="flex flex-col grow gap-3 max-w-2xl p-4">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+
+      {/* Suggested Users */}
+      <SuggestedUsersCard />
+    </div>
+  );
+};
+
+export { BookmarksPage };
