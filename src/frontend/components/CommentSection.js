8fc243984da93793187e260e112f5e5f5d80c3a2
MarkVed17 <vedantlahane17@gmail.com>
2022-06-25 05:01:51 +0530

    style: changes wrt light-mode

--- src/frontend/components/CommentSection.js
+++ src/frontend/components/CommentSection.js
@@ -5,7 +5,7 @@ const CommentSection = () => {
     <div className="flex items-center">
       <div className="relative mr-3 w-full">
         <input
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Add your comment..."
           type="text"
           name="comment"
