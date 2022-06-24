e3313d2bc5414f84ae9f4fbfe8c947c737608d30
MarkVed17 <vedantlahane17@gmail.com>
2022-06-25 05:02:46 +0530

    feat: added `Toaster` for notifs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Footer, Header, Main } from "./frontend/layouts";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="App">
+      <Toaster position="bottom-center" reverseOrder={true} />
       <Header />
       <Main />
       <Footer />
