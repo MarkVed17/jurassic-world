0ba5371884d876cead5420c53cd622cdf8377796
MarkVed17 <vedantlahane17@gmail.com>
2022-06-29 01:09:13 +0530

    barrel exports services

---
+++ src/frontend/services/posts-services/index.js
@@ -0,0 +1,8 @@
+export { getAllPostsService } from "./getAllPostsService";
+export { getUserPostService } from "./getUserPostService";
+export { getAllUserPostsService } from "./getAllUserPostsService";
+export { createPostService } from "./createPostService";
+export { deletePostService } from "./deletePostService";
+export { editPostService } from "./editPostService";
+export { likePostService } from "./likePostService";
+export { dislikePostService } from "./dislikePostService";
