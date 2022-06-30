fa6617535077664a96473bbeee78a6dee4c8654c
MarkVed17 <vedantlahane17@gmail.com>
2022-06-30 15:32:27 +0530

    updated Single Post Page

--- src/frontend/pages/SinglePostPage/SinglePostPage.js
+++ src/frontend/pages/SinglePostPage/SinglePostPage.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import {
-  Comment,
   CommentSection,
   Post,
   Sidebar,
   SuggestedUsersCard,
 } from "../../components";
+import { getUserPost } from "../../features";
 
 const SinglePostPage = () => {
+  const { data: post } = useSelector((state) => state.post);
+  const { data: posts } = useSelector((state) => state.posts);
+
+  const dispatch = useDispatch();
+  const { postId } = useParams();
+
+  useEffect(() => {
+    dispatch(getUserPost(postId));
+  }, [dispatch, postId, posts]);
+
   return (
     <div className="main-content justify-evenly dark:bg-gray-800">
       {/* Sidebar */}
@@ -16,14 +28,16 @@ const SinglePostPage = () => {
       {/* Center Content */}
       <div className="flex flex-col grow max-w-2xl p-4">
         <div className="flex flex-col gap-3">
-          <Post />
-          <CommentSection />
-          {/* Comments */}
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
+          {post === null ? (
+            <p className="text-black text-xl m-auto dark:text-white">
+              Oops! Couldn't find what you're looking for...
+            </p>
+          ) : (
+            <>
+              <Post post={post} />
+              <CommentSection postId={postId} comments={post.comments} />
+            </>
+          )}
         </div>
       </div>
 
