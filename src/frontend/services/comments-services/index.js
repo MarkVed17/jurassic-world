b8ffe046c4ae6fa166249ddbc4b23a07d471525d
MarkVed17 <vedantlahane17@gmail.com>
2022-06-30 23:44:29 +0530

    barrel exported services

---
+++ src/frontend/services/comments-services/index.js
@@ -0,0 +1,4 @@
+export { getPostCommentsService } from "./getPostCommentsService";
+export { addPostCommentService } from "./addPostCommentService";
+export { deletePostCommentService } from "./deletePostCommentService";
+export { editPostCommentService } from "./editPostCommentService";
