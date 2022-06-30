f58ea0372cb7aed19b1c6237c4d01a12865579ef
MarkVed17 <vedantlahane17@gmail.com>
2022-06-30 15:34:47 +0530

    feat: added `post` & `user` reducers to store

--- src/frontend/store.js
+++ src/frontend/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, postsReducer } from "./features";
+import { authReducer, postReducer, postsReducer, userReducer } from "./features";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     posts: postsReducer,
+    post: postReducer,
+    user: userReducer,
   },
 });
 
