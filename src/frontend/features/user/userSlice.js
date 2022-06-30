135954aa9cedd5c02528ff2c1a630429612a7a21
MarkVed17 <vedantlahane17@gmail.com>
2022-06-30 15:33:21 +0530

    feat: added `Get a user & its posts` actions

---
+++ src/frontend/features/user/userSlice.js
@@ -0,0 +1,68 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getAllUserPostsService, getUserService } from "../../services";
+
+const userInitialState = {
+  profile: {
+    data: null,
+    loading: false,
+  },
+  posts: {
+    data: null,
+    loading: false,
+  },
+};
+
+const getUser = createAsyncThunk(
+  "user/getUser",
+  async (username, { rejectWithValue }) => {
+    try {
+      const { data } = await getUserService(username);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data.errors[0]);
+    }
+  }
+);
+
+const getAllUserPosts = createAsyncThunk(
+  "user/getAllUserPosts",
+  async (username, { rejectWithValue }) => {
+    try {
+      const { data } = await getAllUserPostsService(username);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data.errors[0]);
+    }
+  }
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: userInitialState,
+  extraReducers: {
+    [getUser.pending]: (state) => {
+      state.profile.loading = true;
+    },
+    [getUser.fulfilled]: (state, action) => {
+      state.profile.loading = false;
+      state.profile.data = action.payload.user;
+    },
+    [getUser.rejected]: (state) => {
+      state.profile.loading = false;
+    },
+    [getAllUserPosts.pending]: (state) => {
+      state.posts.loading = true;
+    },
+    [getAllUserPosts.fulfilled]: (state, action) => {
+      state.posts.loading = false;
+      state.posts.data = action.payload.posts;
+    },
+    [getAllUserPosts.rejected]: (state) => {
+      state.posts.loading = false;
+    },
+  },
+});
+
+const userReducer = userSlice.reducer;
+
+export { getAllUserPosts, getUser, userReducer };
