ccc1d78380a0110ca3e22d76707d7f10cc5dbc31
MarkVed17 <vedantlahane17@gmail.com>
2022-06-30 15:26:58 +0530

    feat: added `Create Post` functionality

--- src/frontend/components/CreatePost.js
+++ src/frontend/components/CreatePost.js
@@ -1,18 +1,61 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import toast from "react-hot-toast";
+import { createPost } from "../features";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
-  const { user } = useSelector((state) => state.auth);
+  const [postData, setPostData] = useState({ content: "" });
+
+  const { user, token } = useSelector((state) => state.auth);
   const { firstName, lastName, username, avatarURL } = user;
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const createPostHandler = async () => {
+    const response = await dispatch(createPost({ postData, token }));
+
+    if (response?.payload?.posts !== undefined) {
+      toast.success("Your post has been created successfully!", {
+        style: {
+          background: "#22c55e",
+          color: "#FFFFFF",
+        },
+      });
+      setPostData({ content: "" });
+    } else {
+      toast.error(`${response.payload}`, {
+        style: {
+          background: "#ef4444",
+          color: "#FFFFFF",
+        },
+      });
+    }
+  };
+
   return (
-    <form className="pt-4 px-4">
+    <form
+      className="pt-4 px-4"
+      onSubmit={(e) => {
+        e.preventDefault();
+        createPostHandler();
+      }}
+    >
       <div className="mb-4 py-4 px-4 w-full bg-white rounded border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
         <div className="flex items-center gap-3 bg-white dark:bg-gray-800">
           <img
-            className="w-10 h-10 rounded-full"
-            src={avatarURL}
+            className="w-10 h-10 rounded-full hover:cursor-pointer"
+            src={
+              avatarURL === null
+                ? `https://ui-avatars.com/api/?name=${firstName}+${lastName}?format=svg`
+                : avatarURL
+            }
             alt={firstName}
+            onClick={(e) => {
+              e.stopPropagation();
+              navigate(`/profile/${username}`);
+            }}
           />
           <div className="font-medium bg-white dark:bg-gray-800 dark:text-white">
             <p>
@@ -29,6 +72,10 @@ const CreatePost = () => {
             rows="5"
             className="block px-0 w-full text-base text-gray-800 bg-white border-0 outline-none dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
             placeholder="What's happening?"
+            value={postData.content}
+            onChange={(e) => {
+              setPostData({ ...postData, content: e.target.value });
+            }}
             required
           ></textarea>
         </div>
