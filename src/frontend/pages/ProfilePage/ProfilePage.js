463b88a3a4928911d08f982e02eaf7b97346022a
MarkVed17 <vedantlahane17@gmail.com>
2022-06-30 15:36:08 +0530

    updated `Profile Page`

--- src/frontend/pages/ProfilePage/ProfilePage.js
+++ src/frontend/pages/ProfilePage/ProfilePage.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import {
+  PostsSection,
   ProfileSection,
   Sidebar,
   SuggestedUsersCard,
 } from "../../components";
+import { getAllUserPosts, getUser } from "../../features";
 
 const ProfilePage = () => {
+  const dispatch = useDispatch();
+  const { username } = useParams();
+
+  const {
+    profile: { data: profile },
+    posts: { data: posts },
+  } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUser(username));
+  }, [dispatch, username]);
+
+  useEffect(() => {
+    dispatch(getAllUserPosts(username));
+  }, [dispatch, username]);
+
   return (
     <div className="main-content justify-evenly dark:bg-gray-800">
       {/* Sidebar */}
@@ -13,14 +33,25 @@ const ProfilePage = () => {
 
       {/* Center Content */}
       <div className="flex flex-col grow max-w-2xl gap-4 p-4">
-        <ProfileSection />
-        <div className="flex flex-col gap-3">
-          {/* <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post /> */}
-        </div>
+        {profile === null ? (
+          <p className="text-black text-xl m-auto dark:text-white">
+            Oops! User doesn't exist
+          </p>
+        ) : (
+          <ProfileSection profile={profile} />
+        )}
+
+        {posts === null || posts.length === 0 ? (
+          <p className="text-black text-xl m-auto dark:text-white">
+            Oops! You haven't posted anything yet...
+          </p>
+        ) : (
+          <>
+            <div className="flex flex-col gap-3">
+              <PostsSection posts={posts} />
+            </div>
+          </>
+        )}
       </div>
 
       {/* Suggested Users */}
