b4148e3f828395029fbba69fbe882d416711d1b3
MarkVed17 <vedantlahane17@gmail.com>
2022-07-01 13:04:25 +0530

    barrel exports

--- src/frontend/services/users-services/index.js
+++ src/frontend/services/users-services/index.js
@@ -1,1 +1,5 @@
 export { getUserService } from "./getUserService";
+export { getAllUsersService } from "./getAllUsersService";
+export { updateUserService } from "./updateUserService";
+export { followUserService } from "./followUserService";
+export { unfollowUserService } from "./unfollowUserService";
