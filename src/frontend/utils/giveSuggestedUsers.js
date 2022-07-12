aef084dce45fd159e289ef3a057d7d7effa8d589
MarkVed17 <vedantlahane17@gmail.com>
2022-07-12 12:29:15 +0530

    feat: added `Suggested Users` utility

---
+++ src/frontend/utils/giveSuggestedUsers.js
@@ -0,0 +1,10 @@
+const giveSuggestedUsers = (users, username, following) => {
+  const followingUsernames = following.map((user) => user.username);
+  
+  return users.filter(
+    (user) =>
+      !followingUsernames.includes(user.username) && user.username !== username
+  );
+};
+
+export { giveSuggestedUsers };
